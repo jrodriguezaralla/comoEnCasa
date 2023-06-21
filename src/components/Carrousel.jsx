e84c3c5fe69fc09132f518f07685e6d2d50f2728
Jonathan Rodriguez Aralla <jrodriguez.aralla@gmail.com>
2023-06-21 15:17:45 -0300

    acomodando imagenes dentro de contenedor

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -8,20 +8,20 @@ import '../styles/carrousel.css';
 const responsive = {
 	superLargeDesktop: {
 		// the naming can be any, depends on you.
-		breakpoint: { max: 4000, min: 1024 },
+		breakpoint: { max: 4000, min: 1550 },
 		items: 5,
 		slidesToSlide: 1,
 	},
 	desktop: {
-		breakpoint: { max: 1024, min: 800 },
+		breakpoint: { max: 1550, min: 960 },
 		items: 3,
 	},
 	tablet: {
-		breakpoint: { max: 800, min: 464 },
+		breakpoint: { max: 960, min: 650 },
 		items: 2,
 	},
 	mobile: {
-		breakpoint: { max: 464, min: 0 },
+		breakpoint: { max: 650, min: 0 },
 		items: 1,
 	},
 };
