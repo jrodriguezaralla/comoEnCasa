770b7a15215f964016e6fa9d089fa149709a918e
Jonathan Rodriguez Aralla <jrodriguez.aralla@gmail.com>
2023-06-22 20:41:18 -0300

    arrancando de nuevo con carousel

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -66,95 +66,54 @@ const Carrousel = ({ images }) => {
 
 export default Carrousel;*/
 
-import React, { useState, useRef } from 'react';
-import Carousel from 'react-multi-carousel';
-import 'react-multi-carousel/lib/styles.css';
+import React, { useRef, useState } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// import required modules
+import { Pagination, Navigation } from 'swiper';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+
 import '../styles/carrousel.css';
 
-const responsive = {
-	superLargeDesktop: {
-		// the naming can be any, depends on you.
-		breakpoint: { max: 4000, min: 1550 },
-		items: 5,
-		slidesToSlide: 1,
-	},
-	desktop: {
-		breakpoint: { max: 1550, min: 960 },
-		items: 3,
-	},
-	tablet: {
-		breakpoint: { max: 960, min: 650 },
-		items: 2,
-	},
-	mobile: {
-		breakpoint: { max: 650, min: 0 },
-		items: 1,
-	},
-};
-
 const Carrousel = ({ images }) => {
-	const finalWidth = 380; // Ancho final deseado después de la transformación
-	const finalHeight = 380; // Alto final deseado después de la transformación
-
-	const calculateScaleTransform = (width, height) => {
-		const scaleX = finalWidth / width;
-		const scaleY = finalHeight / height;
-		return Math.min(scaleX, scaleY);
-	};
-
-	const [activeIndex, setActiveIndex] = useState(2);
-	const carouselRef = useRef(null);
-
-	const handleNextSlide = () => {
-		carouselRef.current.next();
-		setActiveIndex((prevIndex) => (prevIndex + 1) % images.length);
-	};
-
-	const handlePrevSlide = () => {
-		carouselRef.current.previous();
-		setActiveIndex((prevIndex) => (prevIndex - 1 + images.length) % images.length);
-	};
-
 	return (
-		<div className="carouselContainer">
-			<Carousel
-				ref={carouselRef}
-				responsive={responsive}
-				infinite={true}
-				centerMode={true}
-				containerClass="carouselContainer"
-				itemClass="carouselItem"
-				renderButtonGroupOutside={true}
-				customButtonGroup={<CustomButtonGroup onPrev={handlePrevSlide} onNext={handleNextSlide} />}
+		<div className="sliderContainer">
+			<Swiper
+				slidesPerView={5}
+				spaceBetween={20}
+				loop={true}
+				pagination={{
+					clickable: true,
+				}}
+				navigation={true}
+				modules={[Pagination, Navigation]}
+				breakpoints={{
+					640: {
+						slidesPerView: 1,
+						spaceBetween: 20,
+					},
+					768: {
+						slidesPerView: 3,
+						spaceBetween: 40,
+					},
+					1024: {
+						slidesPerView: 5,
+						spaceBetween: 50,
+					},
+				}}
 			>
-				{images.map((image, index) => (
-					<div key={index} className="imgContainer">
-						<img
-							src={image}
-							alt=""
-							style={{
-								width: '305.3px',
-								height: '310px',
-								transform: index === activeIndex ? `scale(${calculateScaleTransform(305.3, 310)})` : 'none',
-								transition: 'transform 0.5s',
-							}}
-						/>
-					</div>
+				{images.map((image) => (
+					<SwiperSlide className="imgContainer">
+						<img src={image} alt="" />
+					</SwiperSlide>
 				))}
-			</Carousel>
+			</Swiper>
 		</div>
 	);
 };
-
-const CustomButtonGroup = ({ onPrev, onNext }) => (
-	<div className="customButtonGroup">
-		<button className="customButton prevButton" onClick={onPrev}>
-			&lt;
-		</button>
-		<button className="customButton nextButton" onClick={onNext}>
-			&gt;
-		</button>
-	</div>
-);
 
 export default Carrousel;
